697034554c35f9125c3c6c59bff01ed99374bcff
dustinfast <dustin.fast@hotmail.com>
2018-11-23 15:02:30 -0600

    Added web template files -DF

--- deploy.py
+++ deploy.py
@@ -12,4 +12,14 @@ app = flask.Flask(__name__)
 def home():
     """ Serves home.html, the main console interface
     """
-    return 'Deploy Test'
+
+    # TODO: Get contents of terminal for web echo
+
+    return flask.render_template('home.html',
+                                 title_string='title_string',
+                                 title_msg='title_msg',
+                                 desc_string='desc_string',
+                                 year_string='year_string',
+                                 author_string='author_string',
+                                 copyright_link='copywrite_link',
+                                 copyright_link_text='copyright_link_text')
